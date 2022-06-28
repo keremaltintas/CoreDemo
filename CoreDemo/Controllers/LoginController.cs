681ca45f500742d538089b15598b7beb5ddf585e
kerem <keremaltintas@posta.mu.edu.tr>
2022-06-28 18:27:33 +0300

    Son eklemelerr yapıldı..

--- CoreDemo/Controllers/LoginController.cs
+++ CoreDemo/Controllers/LoginController.cs
@@ -1,12 +1,45 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace CoreDemo.Controllers
 {
     public class LoginController : Controller
     {
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task <IActionResult> Index(Writer p)
+        {
+            Context c= new Context();
+            var datavalue=c.Writers.FirstOrDefault(x=>x.WriterMail== p.WriterMail && x.WriterPassword == p.WriterPassword);
+            if(datavalue!=null)
+            {
+                return RedirectToAction("Index", "Writer");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
+//Context c = new Context();
+//var datavalue = c.Writers.FirstOrDefault(x => x.WriterMail == p.WriterMail && x.WriterPassword == p.WriterPassword);
+//if (datavalue != null)
+//{
+//    HttpContext.Session.SetString("username", p.WriterMail);
+//    return RedirectToAction("Index", "Writer");
+//}
+//else
+//{
+//    return View();
+//}
---
+++ CoreDemo/Controllers/WriterController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class WriterController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+    }
+}
